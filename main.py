61bc0aa5ba84a55e033812276c38478d5cd9ee26
shriram243 <shriram.rawat2@flipkart.com>
2022-08-04 10:49:54 +0530

    ignore commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run('app:app', host="127.0.0.1", port=5000, reload=True)
-    # uvicorn.run('app:app', host="172.29.195.37", port=5000, reload=True)
+    # uvicorn.run('app:app', host="127.0.0.1", port=5000, reload=True)
+    uvicorn.run('app:app', host="172.29.199.87", port=5000, reload=True)
